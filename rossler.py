959ff97422c7c1b3831c552a0b9d68eb70841033
hossen-code <hghayoor@gmail.com>
2019-10-21 20:31:49 -0400

    some changes regarding rossler function

--- rossler.py
+++ rossler.py
@@ -1,16 +1,18 @@
-function dy=rossler(t,y)
-t
-N=round(length(y)/3);
-dy=zeros(3*N,1);
 
-%parameters
-% a=.2; b=.2; c=5.7;
-a=.38; b=.3; c=4.82;
- 
-x1=y(1:N,1);
-x2=y(N+1:2*N,1);
-x3=y(2*N+1:3*N,1);
+import numpy as np
 
-dy(1:N,1)      = -x2-x3;
-dy(N+1:2*N,1)  = x1+a*x2;
-dy(2*N+1:3*N,1)= b+x3.*(x1-c);
+def rossler(t:float, y:float):
+    n = np.round(len(y) / 3) # why len of y
+    
+    a = 0.38 # 0.2
+    b = 0.3 # 0.2
+    c = 4.82 # 5.7
+    # use numpy array
+    x_1 = y[0:n]
+    x_2 = y[n:2*n]
+    x_3 = y[2*n:3*n]
+    
+    dy = np.zeros(1, len(y))
+    dy[0:n] = - x_1 - x_3
+    dy[n:2*n] = x_1 + a * x_2
+    dy[2*n:3*n] = b + x_3 * (x_1 - c)
