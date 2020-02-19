565bc8a221f32e8bbfdf7b1cd18a149346478288
Hossein Ghayoor <hosseing@stradigi.ai>
2020-02-18 19:22:23 -0500

    made the dataset function

--- extreme_events/data_source/data_providers.py
+++ extreme_events/data_source/data_providers.py
@@ -1,11 +1,19 @@
-from rossler import rossler
+from extreme_events.data_source.rossler import rossler
 
 import numpy as np
 from scipy.integrate import odeint
 
 # numerical integration
-x_0 = [2, 2, 2]
-time = np.arange(0, 300, 0.01)
-result = odeint(rossler, x_0, time)
-x, y, z = result.T
+X_0 = [2, 2, 2]
+TIME = np.arange(0, 300, 0.01)
 
+
+def rossler_dataset(x_0, time_range: np.arange):
+    result = odeint(rossler, x_0, time_range)
+    x, y, z = result.T
+
+    return x, y, z
+
+
+if __name__ == "__main__":
+    data = rossler_dataset(x_0=X_0, time_range=TIME)
