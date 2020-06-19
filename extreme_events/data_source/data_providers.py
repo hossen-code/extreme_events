fc3c13bb96274a9e3fb5a21fd9885012251dea35
hossen-code <hghayoor@gmail.com>
2020-06-19 15:12:01 -0400

    ops added

--- extreme_events/data_source/data_providers.py
+++ extreme_events/data_source/data_providers.py
@@ -1,21 +1,26 @@
 from collections import namedtuple
 from math import floor
+from typing import List
 
 import numpy as np
 from scipy.integrate import odeint
 
 from extreme_events.data_source.rossler import rossler
 
-X_0 = [2, 2, 2]
-TIME = np.arange(0, 300, 0.1)
-
-data = namedtuple("data", ["feature", "target"])
+data_format = namedtuple("data", ["feature", "target"])
 
 
-def rossler_dataset(x_0, time_range: np.arange):
+def rossler_dataset_maker(x_0: List[float], time_range: np.arange) -> data_format:
+    """
+    return the dataset in x, y, z format. z is the target.
+    :param x_0: is the initial point [x0, y0, z0]
+    :param time_range: is numpy range e.g. np.arange(0, 300, 0.1)
+        from 0 to 300, with the step 0.1
+    :return: data to train
+    """
     result = odeint(rossler, x_0, time_range)
     x, y, z = result.T
-    return data((x, y), (z,))
+    return data_format((x, y), (z,))
 
 
 def train_test_splitter(data_column: np.ndarray,
@@ -28,8 +33,3 @@ def train_test_splitter(data_column: np.ndarray,
     test_set = data_column[train_size:]
 
     return train_set, test_set
-
-
-if __name__ == "__main__":
-    data = rossler_dataset(x_0=X_0, time_range=TIME)
-    pass
