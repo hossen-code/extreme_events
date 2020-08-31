bf591266e268dbdf6c1dc22d9c1fcbff42817656
hossein <hghayoor@gmail.com>
2020-08-31 16:05:53 -0400

    changed the data_provider to comply to standard data shape of ts

--- extreme_events/data_source/data_providers.py
+++ extreme_events/data_source/data_providers.py
@@ -10,22 +10,28 @@ from extreme_events.data_source.rossler import rossler
 data_format = namedtuple("data", ["feature", "target"])
 
 
-def rossler_dataset_maker(x_0: List[float], time_range: np.arange) -> data_format:
+def rossler_dataset_maker(x_0: List[float], time_range: np.arange) -> np.ndarray:
     """
-    return the dataset in x, y, z format. z is the target.
+    return the dataset in x, y, z format of float32 numbers. z is the target.
     :param x_0: is the initial point [x0, y0, z0]
     :param time_range: is numpy range e.g. np.arange(0, 300, 0.1)
         from 0 to 300, with the step 0.1
-    :return: data to train
+    :return: data to train, data.shape is (a, b, c) which a is the number of batches
+    b is the time series (values through time), c is the number of variates + target
+    (for now we assume, the last one is the target as our problem is single-target).
+    the array shape, is to comply with standard deep learning models.
     """
     result = odeint(rossler, x_0, time_range)
     x, y, z = result.T
-    return data_format((x, y), (z,))
+    stacked_and_converted = np.float32(np.stack([x, y, z], axis=1))
+    final_shaped_array = np.expand_dims(stacked_and_converted, axis=0)
+    return final_shaped_array
 
 
 def train_test_splitter(data_column: np.ndarray,
                         train_ratio: float):
     """
+    TODO: this needs to change, to be vertical split (or have the option)
     for now we're assuming the last member is the target.
     """
     train_size = floor(len(data_column)*train_ratio)
--- extreme_events/models/lstm.py
+++ extreme_events/models/lstm.py
@@ -13,34 +13,37 @@ def set_seeds_and_clear_session():
 
 
 def make_lstm_model():
-    model = keras.models.Sequential(
-        [keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
-         # keras.layers.LSTM(20, return_sequences=True),
-         # keras.layers.TimeDistributed(keras.layers.Dense(10))
-         ])
-    optimizer = keras.optimizers.SGD(lr=1e-8, momentum=0.9)
-    model.compile(loss=keras.losses.Huber(),
-                  optimizer=optimizer,
-                  metrics=["mse"])
+
+    def last_time_step_mse(Y_true, Y_pred):
+        return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
+
+    model = keras.models.Sequential([
+        keras.layers.LSTM(20, return_sequences=True, input_shape=[None, 1]),
+        keras.layers.LSTM(20, return_sequences=True),
+        keras.layers.TimeDistributed(keras.layers.Dense(10))
+    ])
+    model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
     return model
 
 
 def simple_rnn_model():
     model = keras.models.Sequential([
         keras.layers.SimpleRNN(1, input_shape=[None, 1])])
-    optimizer = keras.optimizers.SGD(lr=1e-8, momentum=0.9)
+    optimizer = keras.optimizers.Adam(lr=0.005)
     model.compile(loss=keras.losses.Huber(),
                   optimizer=optimizer,
                   metrics=["mse"])
     return model
 
 
-def run_model(train_data):
+def run_model(x_train, y_train, x_valid=None, y_valid=None):
     # lstm = make_lstm_model()
     lstm = simple_rnn_model()
     lr_schedule = keras.callbacks.LearningRateScheduler(
         lambda epoch: 1e-8 * 10**(epoch / 20))
-    history = lstm.fit(train_data, epochs=10, callbacks=[lr_schedule])
+    with tf.device("/GPU:0"):
+        history = lstm.fit(x_train, y_train, epochs=20) # run the lstm model with our data
+    # history = lstm.fit(train_data, epochs=10, callbacks=[lr_schedule])
 
     return history
 
@@ -58,5 +61,5 @@ if __name__ == "__main__":
     x_0 = [2, 2, 2]
     time_range = np.arange(0, 30, 0.1)
     data = rossler_dataset_maker(x_0=x_0, time_range=time_range)
-    train, test = train_test_splitter(data.feature[0], train_ratio=0.8)
-    run_model(train)
+    # train_data, test_data = train_test_splitter(data.feature[0], train_ratio=0.8)
+    run_model(data.target[0], data.feature[0])
